d9efc19a5f8306a00772d9c6867f9f6372cf4f76
ImAClownAndDontComplain <lukporey1317@bk.ru>
2022-05-23 20:01:59 +0500

    start version

---
+++ 28/random_texture.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <GL/glew.h>
+
+class RandomTexture
+{
+public:
+    RandomTexture();
+
+    ~RandomTexture();
+    
+    bool InitRandomTexture(unsigned int Size);
+
+    void Bind(GLenum TextureUnit);
+
+private:
+    GLuint m_textureObj;
+};
+
+
